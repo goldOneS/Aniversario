b27ee93c417b15e1901b176d44af84da42a586a4
goldOneS <71994333+goldOneS@users.noreply.github.com>
2026-08-06 14:47:20 -0600

    Update scripts.js

--- mi-amor/scripts.js
+++ mi-amor/scripts.js
@@ -179,7 +179,7 @@ async function cargarPoema() {
   const poemaEl = document.getElementById("poema");
   if (!poemaEl) return;
   try {
-    const res = await fetch("/api/poema.js");
+    const res = await fetch("/api/poema"); // 👈 sin ".js"
     const data = await res.json();
     poemaEl.textContent = data.poema.trim();
   } catch (err) {
@@ -187,7 +187,6 @@ async function cargarPoema() {
   }
 }
 cargarPoema();
-
 /* ============================================
    BUZÓN SINCERO (vía /api/queja)
    ============================================ */
